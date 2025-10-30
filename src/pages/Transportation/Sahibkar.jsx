2770bffb3cf210c2f575c91615705f8fc27755ac
Hpt9 <106389685+Hpt9@users.noreply.github.com>
2025-10-30 17:05:51 +0400

    Update Sahibkar.jsx

--- src/pages/Transportation/Sahibkar.jsx
+++ src/pages/Transportation/Sahibkar.jsx
@@ -34,25 +34,25 @@ const SahibkarIndex = () => {
   const [truckTypeId, setTruckTypeId] = useState("");
   const [capacity, setCapacity] = useState("");
 
-  useEffect(() => {
-    setSearchBar(
-      <div className="relative w-full md:w-[300px] pl-[16px] hidden md:block">
-        <input
-          type="text"
-          placeholder={language === 'az' ? 'Axtar' : language === 'en' ? 'Axtar' : 'Поиск'}
-          className="w-full px-4 py-2 border border-[#E7E7E7] rounded-lg focus:outline-none focus:border-[#2E92A0] text-[#3F3F3F]"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="absolute right-7 top-1/2 transform -translate-y-1/2">
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#A0A0A0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-          </svg>
-        </button>
-      </div>
-    );
-    return () => setSearchBar(null);
-  }, [setSearchBar, searchQuery, language]);
+  // useEffect(() => {
+  //   setSearchBar(
+  //     <div className="relative w-full md:w-[300px] pl-[16px] hidden md:block">
+  //       <input
+  //         type="text"
+  //         placeholder={language === 'az' ? 'Axtar' : language === 'en' ? 'Axtar' : 'Поиск'}
+  //         className="w-full px-4 py-2 border border-[#E7E7E7] rounded-lg focus:outline-none focus:border-[#2E92A0] text-[#3F3F3F]"
+  //         value={searchQuery}
+  //         onChange={(e) => setSearchQuery(e.target.value)}
+  //       />
+  //       <button className="absolute right-7 top-1/2 transform -translate-y-1/2">
+  //         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  //           <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="#A0A0A0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+  //         </svg>
+  //       </button>
+  //     </div>
+  //   );
+  //   return () => setSearchBar(null);
+  // }, [setSearchBar, searchQuery, language]);
 
   // Fetch filter lists
   useEffect(() => {
