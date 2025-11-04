0e6ce9d0055d4f8ca8e838df198e523aa232e7dd
Hpt9 <feridsattarov5@gmail.com>
2025-11-04 11:11:11 +0400

    feat: footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,9 @@ const Footer = () => {
     <div className="w-full flex justify-center">
       <footer className="bg-[#FAFAFA] text-white py-[16px] px-[16px] md:px-[32px] lg:px-[50px] xl:px-[108px] w-full flex justify-center">
       <div className="flex flex-col-reverse md:flex-row gap-y-[32px] md:gap-y-0 justify-between items-center w-full max-w-[1920px]">
-        <p className=" text-center text-[rgba(63,63,63,1)]">Bütün hüquqlar qorunur - 2025</p>
+        <p className="text-center text-[rgba(63,63,63,1)]">
+          Bütün hüquqlar qorunur - {new Date().getFullYear()}
+        </p>
         <div className="flex items-center gap-x-[24px] lg:gap-x-[32px] xl:gap-x-[64px]">
           <img src={KOB_LOGO} alt="KOB LOGO" className="w-[40px] h-[55px] lg:w-[55px] lg:h-[70px] hover:cursor-pointer " onClick={() => window.open("https://www.smb.gov.az/az", "_blank")}/>
           <img src={AGTL_LOGO} alt="AGTL LOGO" className="w-[55px] h-[49px] lg:w-[70px] lg:h-[64px] hover:cursor-pointer" onClick={() => window.open("https://agtl.org.az/", "_blank")}/>
@@ -20,7 +22,7 @@ const Footer = () => {
             className="w-[95px] h-[25px] lg:w-[110px] lg:h-[40px] hover:cursor-pointer"
             onClick={() => window.open("https://pasha-holding.az/", "_blank")}
           />
-          <img src={mttm} alt="MTTM LOGO" className="w-[60px] h-[40px] hover:cursor-pointer " onClick={() => window.open("https://mttm.org.az/", "_blank")}/>
+          <img src={mttm} alt="MTTM LOGO" className="w-[60px] h-[70px] hover:cursor-pointer " onClick={() => window.open("https://mttm.org.az/", "_blank")}/>
         </div>
         <div className="social_medias flex items-center gap-x-[16px]">
           <div className="w-[40px] h-[40px] rounded-full border border-[#E7E7E7] flex items-center justify-center">
